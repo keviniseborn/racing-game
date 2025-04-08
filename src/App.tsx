159618313b6e47301a203fc8f434ff8bfdf9e6d9
Kevin Iseborn <keviniseborn@users.noreply.github.com>
2025-04-08 18:35:02 +0100

    Initial setup of Vite, React, TS and ThreeJs

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import ThreeScene from './components/ThreeScene';
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <ThreeScene />
+      </div>
+  )
+}
+
+export default App
